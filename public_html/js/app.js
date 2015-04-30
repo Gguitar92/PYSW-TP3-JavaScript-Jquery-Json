8dbe637151ea576400600ed735b619dd87cbcf2b
Mat-iaS <mts1788@gmail.com>
2015-04-30 06:17:44 -0300

    punto 1 y 2
    [+] validacion de matricula y mensaje
    [+] comboBox especialidad
    [+] checkBox busqueda avanzada

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -6,22 +6,45 @@
 
 var App = App || {};
 App.Practico = (function() {
+    
     var validarMatricula = function(){
         $("#matricula").change(function (){
-            alert($(this).val());
+            var validatorMatricula =new RegExp("^([A-Z]|[a-z]){2}\\u002D\\d{4}$");
+            console.log(validatorMatricula.test($(this).val())+" - mat:"+" "+$(this).val())
+            if (!validatorMatricula.test($(this).val())){
+                mostrarMsg("la Matricula es incorrecta");
+            }
         });
     };
     
+    var mostrarMsg=function (mensaje){
+        $("#mensaje").hide();
+        $("#mensaje").fadeIn(2000);
+        $("#mensaje").html("<h4>"+mensaje+"</h4>");
+        $("#mensaje").fadeOut(2000);
+    }
+    
     return {
         inicio:function (){
-            alert('app iniciado');  
+            console.log('app iniciado');  
         },
         punto1:function (){
             validarMatricula();
         },
+        punto2:function (){
+            $("#chkBusqAvanzada").change(function (){
+                console.log("checked: "+$(this).is(":checked"));
+                if($(this).is(":checked")){
+                    $("#especialidad").attr("hidden",false);
+                }else{
+                    $("#especialidad").attr("hidden",true);
+                }
+            })
+        },
         init: function() {
             App.Practico.inicio();
             App.Practico.punto1();
+            App.Practico.punto2();
         }
     };
 })();
