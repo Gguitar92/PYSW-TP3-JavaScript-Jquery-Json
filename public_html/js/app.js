52a005f215a29982f09080d169eb284c9552c29d
Mat-iaS <mts1788@gmail.com>
2015-05-02 03:08:26 -0300

    punto 4 busqueda
    [+] Busqueda y agregado de resultados a la tabla

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -27,6 +27,16 @@ App.Practico = (function() {
         $("#mensaje").fadeOut(2000);
     }
     
+    //agrega un profesional a la tabla de profesionales encontrados
+    var agregarFila=function (p){
+        $('#tblProfesionales tr:last').after('<tr><td>'+p.apellido+'</td>'+
+                                '<td>'+p.nombres+'</td>'+
+                                '<td>'+p.email+'</td>'+
+                                '<td>'+p.matricula+'</td>'+
+                                '<td>'+p.fechaMatriculacion+'</td>'+
+                                '<td>'+p.ambito+'</td>'+
+                                '<td>'+p.especialidad+'</td>'+'</tr>');
+    }
     return {
         inicio : function (){
             console.log('app inicio');  
@@ -48,7 +58,7 @@ App.Practico = (function() {
         punto3 : function (){
 //            console.log("-------punto3")
             $.getJSON('profesionales.json',function(json){
-//                console.log(json);
+                console.log("arrayProf: "+json);
                 //cargo el array con los datos del archivo json
                 arrayProfesionales=json.profesionales; //profesionales es un array en el archivo .json
                 
@@ -59,11 +69,50 @@ App.Practico = (function() {
 //                });
             });
         },
+        punto4 : function (){
+          $("#btnBuscar").click(function(){
+                console.log("click buscar");
+
+                var matricula = $("#txtMatricula").val();
+                var nombres = $("#txtNombre").val();
+                var apellido = $("#txtApellido").val();
+                var email = $("#txtMatricula").val();
+                var ambito="n";
+                var especialidad=$('#cmbEspecialidad').val();
+                if ($("#radioProv").is(":checked")){
+                    ambito = "p";
+                }
+                
+                $("#tblProfesionales td").remove(); //limpio la tabla
+                
+                //recorro el array y comparo con los datos ingresados
+                $.each(arrayProfesionales,function (i,p){
+                    if (matricula != "" && p.matricula == matricula){
+                        agregarFila(p);
+                    }else{
+                        if($("#chkBusqAvanzada").is(":checked") && p.especialidad==especialidad){
+                            agregarFila(p);
+                        }else{
+                            if(!$("#chkBusqAvanzada").is(":checked") && ambito==p.ambito){
+                                agregarFila(p);
+                            }else{
+                                if((nombres != "" && p.nombres.toLowerCase().contains(nombres)) 
+                                        || (apellido != "" && p.apellido.toLowerCase().contains(apellido))
+                                        || (email != "" && p.email==email)){
+                                    agregarFila(p);
+                                }                            
+                            }
+                        }
+                    }
+                });
+          });
+        },
         init: function() {
             App.Practico.inicio();
             App.Practico.punto1();
             App.Practico.punto2();
             App.Practico.punto3();
+            App.Practico.punto4();
         }
     };
 })();
